46e5c87b2ecf006e3837afc56c95ac9425f604f9
amandeeppal0001 <amandeeppal161@gmail.com>
2026-08-02 10:41:36 +0530

    update

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -153,8 +153,8 @@ export const loginUser = asyncHandler(async (req, res) => {
 
        const options = {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production', 
-    sameSite: process.env.NODE_ENV === 'production' ? "none" : "lax" 
+    secure: true, 
+    sameSite:  "none" 
 };
 
 
